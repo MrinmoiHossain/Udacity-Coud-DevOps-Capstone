cd2028182bbd9548916b46640536a0e4ba3de227
MrinmoiHossain <mokit.aust@gmail.com>
2020-05-31 18:56:29 +0600

    home page context update

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to the project"
+    html = f"<h2>Welcome to the Udacity Cloud DevOps Capstone Project</h3>"
+    return html.format(format)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
